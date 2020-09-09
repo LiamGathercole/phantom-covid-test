bd42da5b5119a7c89dfcb15d7d977db59fb05f24
LiamGathercole <liam.gathercole@gmail.com>
2020-09-09 18:57:05 +0100

    CSV parsed to json

--- src/App.js
+++ src/App.js
@@ -2,12 +2,22 @@ import React from "react";
 import { readRemoteFile } from "react-papaparse";
 
 import logo from "./logo.svg";
+import csvData from "./phe_cases_london_boroughs.csv";
 import "./App.css";
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {};
+  }
+
+  componentDidMount() {
+    readRemoteFile(csvData, {
+      header: true,
+      complete: (results) => {
+        console.log("Results:", results);
+      },
+    });
   }
 
   render() {
