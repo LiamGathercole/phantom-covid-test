3fe50cc746defb8cb1b5b5d3ffbae1cb19082fe2
LiamGathercole <liam.gathercole@gmail.com>
2020-09-09 18:44:34 +0100

    eslint and prettier added to project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { readRemoteFile } from "react-papaparse";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
+import logo from "./logo.svg";
+import "./App.css";
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="App">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      </div>
+    );
+  }
 }
 
 export default App;
