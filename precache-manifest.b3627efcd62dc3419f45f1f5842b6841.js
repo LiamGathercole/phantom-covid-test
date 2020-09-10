9fef21d867545313f9a310ed84f4056f4f3fb293
liamgathercole <liam.gathercole@gmail.com>
2020-09-10 21:18:34 +0100

    Updates

---
+++ precache-manifest.b3627efcd62dc3419f45f1f5842b6841.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5d13148e237a139c7621f3c1bf27f41",
+    "url": "/phantom-covid-test/index.html"
+  },
+  {
+    "revision": "cf42a9d59c8b9fb1e42f",
+    "url": "/phantom-covid-test/static/css/main.d38bd98e.chunk.css"
+  },
+  {
+    "revision": "a449a27021a2e473027a",
+    "url": "/phantom-covid-test/static/js/2.acc8b786.chunk.js"
+  },
+  {
+    "revision": "21efbe1489395e060d7887170c611285",
+    "url": "/phantom-covid-test/static/js/2.acc8b786.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf42a9d59c8b9fb1e42f",
+    "url": "/phantom-covid-test/static/js/main.28401f80.chunk.js"
+  },
+  {
+    "revision": "16d49321265b4e5890c1",
+    "url": "/phantom-covid-test/static/js/runtime-main.09bce3e9.js"
+  },
+  {
+    "revision": "ab53553657d74bfd34e4710c0da08002",
+    "url": "/phantom-covid-test/static/media/covid-logo.ab535536.png"
+  },
+  {
+    "revision": "368a4796f81d68535cfda2015a41742d",
+    "url": "/phantom-covid-test/static/media/phe_cases_london_boroughs.368a4796.csv"
+  }
+]);
